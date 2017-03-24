6f12c30121a9dbfb38bd4fd4770e43de375d88ce
Johan Sageryd <johan.sageryd@bonnierbroadcasting.com>
2017-03-24 17:10:48 +0100

    Split TestSubset -> Test{Asset,Series}_Subset
    Split the two sub-tests of TestSubset into two root-level test functions
    to allow adding another level of sub-tests without additional nesting.

--- response_test.go
+++ response_test.go
@@ -2,38 +2,36 @@ package search
 
 import "testing"
 
-func TestSubset(t *testing.T) {
-	t.Run("Asset", func(t *testing.T) {
-		a := &Asset{
-			VideoID: "video-id-123",
-			Type:    "movie",
-		}
+func TestAsset_Subset(t *testing.T) {
+	a := &Asset{
+		VideoID: "video-id-123",
+		Type:    "movie",
+	}
 
-		sub := Hit(a).Subset()
+	sub := Hit(a).Subset()
 
-		if got, want := sub.ID, a.VideoID; got != want {
-			t.Fatalf("sub.ID = %q, want %q", got, want)
-		}
+	if got, want := sub.ID, a.VideoID; got != want {
+		t.Fatalf("sub.ID = %q, want %q", got, want)
+	}
 
-		if got, want := sub.Type, a.Type; got != want {
-			t.Fatalf("sub.Type = %q, want %q", got, want)
-		}
-	})
+	if got, want := sub.Type, a.Type; got != want {
+		t.Fatalf("sub.Type = %q, want %q", got, want)
+	}
+}
 
-	t.Run("Series", func(t *testing.T) {
-		s := &Series{
-			BrandID: "brand-id-345",
-			Type:    "series",
-		}
+func TestSeries_Subset(t *testing.T) {
+	s := &Series{
+		BrandID: "brand-id-345",
+		Type:    "series",
+	}
 
-		sub := Hit(s).Subset()
+	sub := Hit(s).Subset()
 
-		if got, want := sub.ID, s.BrandID; got != want {
-			t.Fatalf("sub.ID = %q, want %q", got, want)
-		}
+	if got, want := sub.ID, s.BrandID; got != want {
+		t.Fatalf("sub.ID = %q, want %q", got, want)
+	}
 
-		if got, want := sub.Type, s.Type; got != want {
-			t.Fatalf("sub.Type = %q, want %q", got, want)
-		}
-	})
+	if got, want := sub.Type, s.Type; got != want {
+		t.Fatalf("sub.Type = %q, want %q", got, want)
+	}
 }
