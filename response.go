a42ca73a443a459fa3b0a954b3bab235e5454fdf
Johan Sageryd <johan.sageryd@bonnierbroadcasting.com>
2017-04-25 15:56:18 +0200

    Re-add GenreDescription{Da,Fi,Nb,Sv} on assets
    Movie and sports assets now have these fields again, since for these
    particular types it makes sense. As a consequence, we add it for all
    asset types, and also re-add them to the shared subset.

--- response.go
+++ response.go
@@ -48,6 +48,10 @@ type HitSubset struct {
 	DescriptionTinySv     string              `json:"description_tiny_sv"`
 	Events                []Event             `json:"events"`
 	ExternalReferences    []ExternalReference `json:"external_references"`
+	GenreDescriptionDa    string              `json:"genre_description_da"`
+	GenreDescriptionFi    string              `json:"genre_description_fi"`
+	GenreDescriptionNb    string              `json:"genre_description_nb"`
+	GenreDescriptionSv    string              `json:"genre_description_sv"`
 	Genres                []Genre             `json:"genres"`
 	KeywordsDa            []Keyword           `json:"keywords_da"`
 	KeywordsFi            []Keyword           `json:"keywords_fi"`
@@ -108,6 +112,10 @@ type Asset struct {
 	EpisodeNumber         int                 `json:"episode_number"`
 	Events                []Event             `json:"events"`
 	ExternalReferences    []ExternalReference `json:"external_references"`
+	GenreDescriptionDa    string              `json:"genre_description_da"`
+	GenreDescriptionFi    string              `json:"genre_description_fi"`
+	GenreDescriptionNb    string              `json:"genre_description_nb"`
+	GenreDescriptionSv    string              `json:"genre_description_sv"`
 	Genres                []Genre             `json:"genres"`
 	HomeTeam              Team                `json:"hometeam"`
 	ItemsPublished        bool                `json:"items_published"`
@@ -179,6 +187,10 @@ func (a *Asset) Subset() *HitSubset {
 		DescriptionTinySv:     a.DescriptionTinySv,
 		Events:                a.Events,
 		ExternalReferences:    a.ExternalReferences,
+		GenreDescriptionDa:    a.GenreDescriptionDa,
+		GenreDescriptionFi:    a.GenreDescriptionFi,
+		GenreDescriptionNb:    a.GenreDescriptionNb,
+		GenreDescriptionSv:    a.GenreDescriptionSv,
 		Genres:                a.Genres,
 		KeywordsDa:            a.KeywordsDa,
 		KeywordsFi:            a.KeywordsFi,
@@ -287,6 +299,10 @@ func (s *Series) Subset() *HitSubset {
 		DescriptionTinySv:     s.DescriptionTinySv,
 		Events:                s.Events,
 		ExternalReferences:    s.ExternalReferences,
+		GenreDescriptionDa:    s.GenreDescriptionDa,
+		GenreDescriptionFi:    s.GenreDescriptionFi,
+		GenreDescriptionNb:    s.GenreDescriptionNb,
+		GenreDescriptionSv:    s.GenreDescriptionSv,
 		Genres:                s.Genres,
 		KeywordsDa:            s.KeywordsDa,
 		KeywordsFi:            s.KeywordsFi,
